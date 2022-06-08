10e34e73d5ed3fa7d3f39f8a3d60aed6c9e2a5fd
Eugene Ivanov <most1360@yandex.ru>
2022-06-08 14:16:41 +0300

    Done

--- app/src/main/java/com/example/coroutinesflow/MainViewModel.kt
+++ app/src/main/java/com/example/coroutinesflow/MainViewModel.kt
@@ -28,7 +28,8 @@ class MainViewModel : ViewModel() {
         }
         activeJob = viewModelScope.launch(Dispatchers.Default) {
             while (running) {
-                counterFlowPI.value = "3.14${count++}"
+
+                counterFlowPI.value = piSpigot(count++)
                 delay(100)
             }
         }
@@ -42,4 +43,53 @@ class MainViewModel : ViewModel() {
         }
     }
 
+    private fun piSpigot(n: Int): String {
+
+        val pi = StringBuilder(n)
+        val boxes = n * 10 / 3
+        val reminders = IntArray(boxes)
+
+        for (i in 0 until boxes) {
+            reminders[i] = 2
+        }
+        var heldDigits = 0
+        for (i in 0 until n) {
+            var carriedOver = 0
+            var sum = 0
+            for (j in boxes - 1 downTo 0) {
+                reminders[j] *= 10
+                sum = reminders[j] + carriedOver
+                val quotient = sum / (j * 2 + 1)
+                reminders[j] = sum % (j * 2 + 1)
+                carriedOver = quotient * j
+            }
+            reminders[0] = sum % 10
+            var q = sum / 10
+
+            if (q == 9) {
+                heldDigits++
+            } else if (q == 10) {
+                q = 0
+                for (k in 1..heldDigits) {
+                    var replaced = pi.substring(i - k, i - k + 1).toInt()
+                    if (replaced == 9) {
+                        replaced = 0
+                    } else {
+                        replaced++
+                    }
+                    pi.deleteCharAt(i - k)
+                    pi.insert(i - k, replaced)
+                }
+                heldDigits = 1
+            } else {
+                heldDigits = 1
+            }
+            pi.append(q)
+        }
+        if (pi.length >= 2) {
+            pi.insert(1, '.')
+        }
+
+        return pi.toString()
+    }
 }
