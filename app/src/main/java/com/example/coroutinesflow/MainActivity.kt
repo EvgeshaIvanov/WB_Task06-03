115871de7c4b811b2813cc319f1dacd8f183f513
Eugene Ivanov <most1360@yandex.ru>
2022-06-08 17:11:08 +0300

    Done

--- app/src/main/java/com/example/coroutinesflow/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesflow/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
+    /*
+    c)Flow основан на корутинах и лучше подходит, чем LiveData, для других слоев приложения,
+     таких как - репозитории, источники данных и т.д.,
+     они не завязаны на платформенные особенности Android и их будет легче тестировать.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
